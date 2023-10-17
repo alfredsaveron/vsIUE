31198105899d729c887669cc9edfa8d0a71b75fd
Alf <57676676+alfredsaveron@users.noreply.github.com>
2023-10-17 13:38:59 +0300

    Create scanf.c

---
+++ weeks/1/examples/scanf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter an Integer: ");
+    scanf("%d", &num); // Integer from the user
+    printf("You just entered: %d\n", num);
+    return 0;
+}
