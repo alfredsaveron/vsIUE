46d22ee4f77ddbb856cd8197fc2382695d1897a1
Alf <57676676+alfredsaveron@users.noreply.github.com>
2023-10-17 19:34:56 +0300

    changes

--- weeks/2/arithmetic.cs
+++ weeks/2/arithmetic.cs
@@ -7,19 +7,19 @@ class ArithmeticOperatorsExample
         int num1 = 10;
         int num2 = 5;
 
-        // Addition (+)
+        // (+)
         int sum = num1 + num2;
         Console.WriteLine($"Addition: {num1} + {num2} = {sum}");
 
-        // Subtraction (-)
+        // (-)
         int difference = num1 - num2;
         Console.WriteLine($"Subtraction: {num1} - {num2} = {difference}");
 
-        // Multiplication (*)
+        // (*)
         int product = num1 * num2;
         Console.WriteLine($"Multiplication: {num1} * {num2} = {product}");
 
-        // Division (/)
+        // (/)
         int quotient = num1 / num2; // Integer division
         Console.WriteLine($"Division (Integer): {num1} / {num2} = {quotient}");
 
@@ -27,7 +27,7 @@ class ArithmeticOperatorsExample
         double divisionResult = (double)num1 / num2;
         Console.WriteLine($"Division (Floating-Point): {num1} / {num2} = {divisionResult}");
 
-        // Modulus (%)
+        // (%)
         int remainder = num1 % num2;
         Console.WriteLine($"Modulus: {num1} % {num2} = {remainder}");
     }
